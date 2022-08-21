297c94f9a0398fdab996c867a215a3ac27e0d1c2
Nicholas Feix <nf@fconsoft.com>
2022-08-22 01:52:04 +0200

    fixed typo

--- HardwareMonitor/__init__.py
+++ HardwareMonitor/__init__.py
@@ -8,6 +8,6 @@ ASSEMBLY_NAME = "LibreHardwareMonitor"
 
 _LIB_PATH = Path(__file__).parent.joinpath("lib").absolute()
 sys.path.insert(0, str(_LIB_PATH))
-__reference__ = clr.AddReference(str(_LIB_PATH / ("ASSEMBLY_NAME" + "Lib.dll")))
+__reference__ = clr.AddReference(str(_LIB_PATH / (ASSEMBLY_NAME + "Lib.dll")))
 
 __import__(ASSEMBLY_NAME)
